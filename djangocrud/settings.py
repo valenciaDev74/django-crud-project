55dc650f3888cd2777707ee8f541b101e63bd32d
valenciaDev74 <jesus.valencia74@yahoo.com>
2026-06-20 07:07:13 -0400

    static root fixed

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
 if not DEBUG:
-    STATIC_ROUTE = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = "login"
